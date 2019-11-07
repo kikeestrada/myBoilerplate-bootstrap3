d70bdc1e93b2860a04ab7e1a036bd68ab250f90a
Enrique Jose Estrada Nunez <kikeestrada@gmail.com>
2019-11-06 21:38:21 -0600

    first load

---
+++ README.md
@@ -0,0 +1,12 @@
+# myBoilerplate
+
+## This is a Gulp Boilerplate to use components as:
+
+* Pug
+* Sass
+* JS (es6)
+* Markdown
+* json-data
+* gh-pages
+
+gh-pages repository: https://kikeestrada.github.io/myBoilerplate
---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import {hello, bye} from "./modules/example"
+
+
+hello();
+bye();
+
+
+(()=>{
+	if (document.body.classList.contains('home')) {
+		// functions here
+	}else if (document.body.classList.contains('page2')) {
+		// functions here
+	}else if (document.body.classList.contains('page3')) {
+		// functions here
+	}
+})();
